e2a1056c71b792d48240ad57346966cee6e180bf
Fathy <fathymohsen47@gmail.com>
2022-09-07 17:46:04 +0200

    Create

---
+++ src/user/pages/Users.js
@@ -0,0 +1,16 @@
+import React from "react";
+import UsersList from "../components/UsersList";
+
+const Users = () => {
+  // const USERS = [
+  //   {
+  //     id: "u1",
+  //     name: "fathy",
+  //     image: "iimg",
+  //     placeCount: 3,
+  //   },
+  // ];
+
+  return <UsersList />;
+};
+export default Users;
